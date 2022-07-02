26c645457e849f2847667001fc219bbe7b269981
tom1236908745 <tom1233908745@gmail.com>
2022-07-02 22:18:32 +0900

    add: タブ周りの実装

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,16 +4,16 @@ import { ReactNode } from "react";
 const Layout = ({ children }: Props) => {
   return (
     <div className="min-h-screen bg-purple-300">
-      <nav className="flex justify-center space-x-4 ">
+      <nav className="flex justify-center space-x-4 pt-6">
         <Link
           href="/"
-          className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+          className="rounded-lg px-3 py-2 font-medium text-slate-700 hover:bg-slate-100 hover:text-slate-900"
         >
           <a>Home </a>
         </Link>
         <Link
           href="/about"
-          className="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900"
+          className="rounded-lg px-3 py-2 font-medium text-slate-700 hover:bg-slate-100 hover:text-slate-900"
         >
           <a>About</a>
         </Link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,23 +18,78 @@ type FilterProps = {
   value: string;
   onChange: (key: string) => void;
 };
+
 const Filter: VFC<FilterProps> = props => {
+  const [checkedList, setCheckedList] = useState<boolean[]>([
+    true,
+    false,
+    false,
+  ]);
   const handleClick = (key: string, e: MouseEvent<HTMLAnchorElement>) => {
+    let i: number = -1,
+      j: number = -1;
+    i = checkedList.indexOf(true);
+    if (key === "ALL") j = 0;
+    if (key === "TODO") j = 1;
+    if (key === "DONE") j = 2;
+    setCheckedList(
+      checkedList.map((_: boolean, index: number) => {
+        if (index == i) {
+          return false;
+        } else if (index === j) {
+          return true;
+        } else return false;
+      })
+    );
+
     e.preventDefault();
     props.onChange(key);
   };
 
+  const clickedClass = "inline-block p-4 bg-violet-700";
+  const notClickedClass =
+    "inline-block  border-b-2 border-transparent p-4 hover:border-violet-300 hover:text-violet-600 dark:hover:text-violet-300";
+
   return (
-    <div className="mt-10 mb-8 space-x-4">
-      <a href="#" onClick={handleClick.bind(null, "ALL")}>
-        All
-      </a>
-      <a href="#" onClick={handleClick.bind(null, "TODO")}>
-        ToDo
-      </a>
-      <a href="#" onClick={handleClick.bind(null, "DONE")}>
-        Done
-      </a>
+    <div className="mx-auto my-8 w-1/2 rounded-2xl border-b border-violet-200 bg-violet-500 dark:border-gray-700">
+      <ul
+        className="-mb-px flex flex-wrap text-center text-sm font-medium"
+        id="myTab"
+      >
+        <li className="role=presentation mr-2 ml-5">
+          <a
+            className={checkedList[0] ? clickedClass : notClickedClass}
+            id="dashboard-tab"
+            type="button"
+            role="tab"
+            onClick={handleClick.bind(null, "ALL")}
+          >
+            全て
+          </a>
+        </li>
+        <li className="mr-2" role="presentation">
+          <a
+            className={checkedList[1] ? clickedClass : notClickedClass}
+            id="dashboard-tab"
+            type="button"
+            role="tab"
+            onClick={handleClick.bind(null, "TODO")}
+          >
+            途中
+          </a>
+        </li>
+        <li className="mr-2" role="presentation">
+          <a
+            className={checkedList[2] ? clickedClass : notClickedClass}
+            id="settings-tab"
+            type="button"
+            role="tab"
+            onClick={handleClick.bind(null, "DONE")}
+          >
+            完了
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
@@ -75,9 +130,10 @@ const Input: VFC<InputProps> = props => {
     }
   };
   return (
-    <div>
+    <div className="flex flex-col">
+      <span>タスクを入力</span>
       <input
-        className="rounded-lg border-x-4 border-violet-300 p-5 opacity-75 "
+        className="mx-auto my-7 w-72 rounded-lg border-x-4 border-violet-300 p-5 opacity-75 "
         type="text"
         placeholder="reactを勉強する"
         value={text}
@@ -114,13 +170,21 @@ const Home = () => {
 
   return (
     <div className="text-center">
-      <div className="m-10 rounded-lg bg-violet-100 p-6 opacity-40 shadow-lg shadow-white">
-        TODO LIST
-      </div>
-      <Input onAdd={handleAdd} />
+      <div className="m-10 p-6 font-mono text-2xl">- TODO LIST -</div>
+
       <Filter value={filters} onChange={handleFilterChange} />
-      <div className="mx-auto w-80 divide-y divide-blue-200 rounded-md border-2 border-blue-300 bg-purple-50 p-5">
+      <Input onAdd={handleAdd} />
+      <div
+        className={`mx-auto w-1/2 divide-y ${
+          displayItems.length > 0 ? "divide-blue-200" : ""
+        } rounded-md border-2 border-violet-300 bg-purple-50 p-5`}
+      >
         <p>項目</p>
+        {displayItems.length === 0 ? (
+          filters !== "ALL" ? (
+            <p className="m-5 pt-4">nothing</p>
+          ) : null
+        ) : null}
         {displayItems.map((item: todoItem) => (
           <TodoItem key={item.key} item={item} onCheck={handleCheck} />
         ))}
